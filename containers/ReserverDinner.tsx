a6556c0b4762df4fede50fa283a9db5d474d29a5
Diwwy20 <sirawit.phaimuang@gmail.com>
2025-12-09 09:28:09 +0700

    chore: update path reservations image

--- containers/ReserverDinner.tsx
+++ containers/ReserverDinner.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { useTranslations } from "next-intl";
-import reservations from "/images/reservations.png";
+import reservations from "../public/images/reservations.png";
 
 import {
   ShoppingCart,
